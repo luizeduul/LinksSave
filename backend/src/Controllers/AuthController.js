9374a88164d9262f7a558a05652348e84963899a
Luiz.Uliana <luizeduardr@gmail.com>
2020-07-01 23:37:35 -0300

    Começando o model de accounts

--- backend/src/Controllers/AuthController.js
+++ backend/src/Controllers/AuthController.js
@@ -1,10 +1,17 @@
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
 module.exports = {
   async login(req, res) {
     return res.json('está nessa rota aki ')
   },
 
   async create(req, res) {
-    return res.json("message")
+    const email = 'ledulzeee@gmail.com';
+    const password = '123456';
+    const hash = bcrypt.hashSync(password, 8);
+    const result = await Account.create({email, password: hash});
+    
+    return res.json(result);
   }
 
 };
